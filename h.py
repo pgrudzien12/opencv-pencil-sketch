21ca12b6f2ee4532673fd032a0d32bbaea70d475
Paweł Grudzień <p.grudzien12@live.com>
2019-09-01 22:39:51 +0200

    some other tests

---
+++ h.py
@@ -0,0 +1,69 @@
+import numpy as np
+import cv2
+
+img_rgb = cv2.imread("data/images/c.PNG")
+img_gray = cv2.imread("data/images/c.PNG", 0)
+targetImage__ = cv2.imread("data/images/target.png", cv2.IMREAD_GRAYSCALE)
+cv2.imshow("oryginal", img_rgb)
+cv2.imshow("targetImage__", targetImage__)
+image = img_rgb
+
+#check if image exists
+if image is None:
+    print("can not find image")
+    sys.exit()
+
+def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=5.0, threshold=0):
+    """Return a sharpened version of the image, using an unsharp mask."""
+    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+    sharpened = float(amount + 1) * image - float(amount) * blurred
+    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+    sharpened = sharpened.round().astype(np.uint8)
+    if threshold > 0:
+        low_contrast_mask = np.absolute(image - blurred) < threshold
+        np.copyto(sharpened, image, where=low_contrast_mask)
+    return sharpened
+
+def first_der(image):
+    # Apply sobel filter along x direction
+    sobelx = cv2.Sobel(image, cv2.CV_32F, 1, 0)
+    # Apply sobel filter along y direction
+    sobely = cv2.Sobel(image,cv2.CV_32F,0,1)
+    gradient = np.sqrt(np.multiply(sobelx, sobelx) + np.multiply(sobely, sobely))
+    cv2.normalize(gradient, 
+                    dst = gradient, 
+                    alpha = 0, 
+                    beta = 1, 
+                    norm_type = cv2.NORM_MINMAX, 
+                    dtype = cv2.CV_32F)
+                    
+    return gradient
+
+hsvImage = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2HSV)
+
+result = hsvImage[:,:,2]
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+#result = cv2.dilate(img_gray, kernel)
+blurred = cv2.bilateralFilter(img_gray, d = 3, sigmaColor = 40, sigmaSpace = 5)
+result = first_der(blurred ) 
+result = np.uint8(255*result)
+
+#Apply bilateralFilter
+
+lapl = cv2.Laplacian(img_gray, cv2.CV_8U, ksize=3, scale=5, delta = 1)
+
+#result = cv2.equalizeHist(lapl) 
+resultA = np.uint8(np.clip(1.5 * result + 0, 0, 255))
+resultB = np.uint8((np.float32(resultA) + lapl)/2.0)
+#result = 255- cv2.dilate(lapl, kernel)
+
+cv2.imshow("pencilsketch",255-lapl)
+cv2.imshow("pencilsketch2",255-resultA)
+cv2.imshow("pencilsketch3",255-resultB)
+
+#press esc to exit the program
+cv2.waitKey(0)
+
+#close all the opened windows
+cv2.destroyAllWindows()
--- instagram.py
+++ instagram.py
@@ -8,21 +8,23 @@ image2 = cv2.imread(DATA_PATH+"images/c.png", 1)
 targetImage__ = cv2.imread(DATA_PATH+"images/target.png", cv2.IMREAD_GRAYSCALE)
 cv2.imshow("targetImage__", targetImage__)
 
-# Apply sobel filter along x direction
-sobelx = cv2.Sobel(image, cv2.CV_32F, 1, 0)
-# Apply sobel filter along y direction
-sobely = cv2.Sobel(image,cv2.CV_32F,0,1)
-gradient = np.sqrt(np.multiply(sobelx, sobelx) + np.multiply(sobely, sobely))
-cv2.normalize(gradient, 
-                dst = gradient, 
-                alpha = 0, 
-                beta = 1, 
-                norm_type = cv2.NORM_MINMAX, 
-                dtype = cv2.CV_32F)
-                
-_, gradient = cv2.threshold(gradient, 0.01,1,cv2.THRESH_TOZERO)
-gradient = 1-gradient
-
+def first_der(image):
+    # Apply sobel filter along x direction
+    sobelx = cv2.Sobel(image, cv2.CV_32F, 1, 0)
+    # Apply sobel filter along y direction
+    sobely = cv2.Sobel(image,cv2.CV_32F,0,1)
+    gradient = np.sqrt(np.multiply(sobelx, sobelx) + np.multiply(sobely, sobely))
+    cv2.normalize(gradient, 
+                    dst = gradient, 
+                    alpha = 0, 
+                    beta = 1, 
+                    norm_type = cv2.NORM_MINMAX, 
+                    dtype = cv2.CV_32F)
+                    
+    _, gradient = cv2.threshold(gradient, 0.01,1,cv2.THRESH_TOZERO)
+    gradient = 1-gradient
+    return gradient
+gradient = first_der(image)
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 cl1 = clahe.apply(np.uint8(gradient*255))
 dst_gray, dst_color = cv2.pencilSketch(image2, sigma_s=60, sigma_r=0.07, shade_factor=0.05)
